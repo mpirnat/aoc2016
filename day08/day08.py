8407bdab32329c1a586a0cd3bcc2237a5dd65f47
Mike Pirnat <mpirnat@gmail.com>
2016-12-08 16:52:47 -0500

    Refactored day08 to class to avoid passing state everywhere

--- day08/day08.py
+++ day08/day08.py
@@ -12,90 +12,78 @@ NUM_ROWS = 6
 NUM_COLS = 50
 
 
-def init_screen(num_rows, num_cols):
-    screen = []
-    for i in range(num_rows):
-        screen.append([False] * num_cols)
-    return screen
+class Screen:
 
+    def __init__(self, rows, cols):
+        self.data = []
+        for i in range(rows):
+            self.data.append([False] * cols)
 
-def do_command(command, screen):
-    function, args = parse_command(command)
-    screen = function(screen, *args)
-    return screen
+    def do_command(self, command):
+        function, args = self.parse_command(command)
+        return function(*args)
 
+    def parse_command(self, command):
+        parts = command.split()
 
-def parse_command(command):
-    parts = command.split()
+        if parts[0] == 'rect':
+            return (self.rect, tuple([int(x) for x in parts[1].split('x')]))
 
-    if parts[0] == 'rect':
-        return (rect, tuple([int(x) for x in parts[1].split('x')]))
+        elif parts[1] == 'column':
+            col = int(parts[2].split('=')[-1])
+            by = int(parts[-1])
+            return (self.rotate_column, (col, by))
 
-    elif parts[1] == 'column':
-        col = int(parts[2].split('=')[-1])
-        by = int(parts[-1])
-        return (rotate_column, (col, by))
+        elif parts[1] == 'row':
+            row = int(parts[2].split('=')[-1])
+            by = int(parts[-1])
+            return (self.rotate_row, (row, by))
 
-    elif parts[1] == 'row':
-        row = int(parts[2].split('=')[-1])
-        by = int(parts[-1])
-        return (rotate_row, (row, by))
+    def rect(self, cols, rows):
+        """
+        rect AxB turns on all of the pixels in a rectangle at the top-left of the
+        screen which is A wide and B tall.
+        """
+        for i in range(rows):
+            for j in range(cols):
+                self.data[i][j] = True
 
+    def rotate_row(self, row, by):
+        """
+        rotate row y=A by B shifts all of the pixels in row A (0 is the top row)
+        right by B pixels. Pixels that would fall off the right end appear at the
+        left end of the row.
+        """
+        for i in range(by):
+            self.data[row].insert(0, self.data[row].pop())
 
-def rect(screen, cols, rows):
-    """
-    rect AxB turns on all of the pixels in a rectangle at the top-left of the
-    screen which is A wide and B tall.
-    """
-    for i in range(rows):
-        for j in range(cols):
-            screen[i][j] = True
+    def rotate_column(self, col, by):
+        """
+        rotate column x=A by B shifts all of the pixels in column A (0 is the left
+        column) down by B pixels. Pixels that would fall off the bottom appear at
+        the top of the column.
+        """
+        column = [row[col] for row in self.data]
 
-    return screen
+        for i in range(by):
+            column.insert(0, column.pop())
 
+        for row in self.data:
+            row[col] = column.pop(0)
 
-def rotate_row(screen, row, by):
-    """
-    rotate row y=A by B shifts all of the pixels in row A (0 is the top row)
-    right by B pixels. Pixels that would fall off the right end appear at the
-    left end of the row.
-    """
-    for i in range(by):
-        screen[row].insert(0, screen[row].pop())
+    def count_lit_pixels(self):
+        return sum(sum(x) for x in self.data)
 
-    return screen
-
-
-def rotate_column(screen, col, by):
-    """
-    rotate column x=A by B shifts all of the pixels in column A (0 is the left
-    column) down by B pixels. Pixels that would fall off the bottom appear at
-    the top of the column.
-    """
-    column = [row[col] for row in screen]
-
-    for i in range(by):
-        column.insert(0, column.pop())
-
-    for row in screen:
-        row[col] = column.pop(0)
-
-    return screen
-
-
-def count_lit_pixels(screen):
-    return sum(sum(x) for x in screen)
-
-
-def display(screen):
-    print('\n'.join(''.join('#' if p else ' ' for p in row) for row in screen))
+    def display(self):
+        print('\n'.join(''.join('#' if p else ' ' for p in row)
+                for row in self.data))
 
 
 if __name__ == '__main__':
     with open('input.txt') as f:
-        screen = init_screen(NUM_ROWS, NUM_COLS)
+        screen = Screen(NUM_ROWS, NUM_COLS)
         for command in f:
-            screen = do_command(command, screen)
-        print("Part 1:", count_lit_pixels(screen))
+            screen.do_command(command)
+        print("Part 1:", screen.count_lit_pixels())
         print("Part 2:")
-        display(screen)
+        screen.display()
--- day08/test.py
+++ day08/test.py
@@ -1,68 +1,68 @@
 #!/usr/bin/env python
 
 import unittest
-from day08 import init_screen
-from day08 import count_lit_pixels
-from day08 import parse_command
-from day08 import rect, rotate_column, rotate_row
+from day08 import Screen
 
 
 class TestInitializingScreen(unittest.TestCase):
 
     def test_inits_screen(self):
-        screen = init_screen(3, 5)
-        self.assertEqual(len(screen), 3)
-        self.assertEqual(len(screen[0]), 5)
+        screen = Screen(3, 5)
+        self.assertEqual(len(screen.data), 3)
+        self.assertEqual(len(screen.data[0]), 5)
 
 
 class TestParsingCommands(unittest.TestCase):
 
     cases = (
-        ('rect 3x2', (rect, (3, 2))),
-        ('rotate column x=1 by 1', (rotate_column, (1, 1))),
-        ('rotate row y=1 by 2', (rotate_row, (1, 2))),
+        ('rect 3x2', ('rect', (3, 2))),
+        ('rotate column x=1 by 1', ('rotate_column', (1, 1))),
+        ('rotate row y=1 by 2', ('rotate_row', (1, 2))),
     )
 
     def test_parses_commands(self):
+        screen = Screen(3, 5)
         for command, expected in self.cases:
-            self.assertEqual(parse_command(command), expected)
+            parsed = screen.parse_command(command)
+            self.assertEqual(parsed[0].__name__, expected[0])
+            self.assertEqual(parsed[1], expected[1])
 
 
 class TestManipulatingScreen(unittest.TestCase):
 
     def test_rect(self):
-        screen = init_screen(3, 7)
-        screen = rect(screen, 3, 2)
-        self.assertEqual(screen,
+        screen = Screen(3, 7)
+        screen.rect(3, 2)
+        self.assertEqual(screen.data,
                 [[True, True, True, False, False, False, False],
                  [True, True, True, False, False, False, False],
                  [False]*7])
 
     def test_rotate_column(self):
-        screen = init_screen(3, 7)
-        screen = rect(screen, 3, 2)
-        screen = rotate_column(screen, 1, 2)
-        self.assertEqual(screen,
+        screen = Screen(3, 7)
+        screen.rect(3, 2)
+        screen.rotate_column(1, 2)
+        self.assertEqual(screen.data,
                 [[True, True, True, False, False, False, False],
                  [True, False, True, False, False, False, False],
                  [False, True, False, False, False, False, False]])
 
     def test_rotate_row(self):
-        screen = init_screen(3, 7)
-        screen = rect(screen, 3, 2)
-        screen = rotate_row(screen, 1, 1)
-        self.assertEqual(screen,
+        screen = Screen(3, 7)
+        screen.rect(3, 2)
+        screen.rotate_row(1, 1)
+        self.assertEqual(screen.data,
                 [[True, True, True, False, False, False, False],
                  [False, True, True, True, False, False, False],
                  [False]*7])
 
     def test_example_input(self):
-        screen = init_screen(3, 7)
-        screen = rect(screen, 3, 2)
-        screen = rotate_column(screen, 1, 1)
-        screen = rotate_row(screen, 0, 4)
-        screen = rotate_column(screen, 1, 1)
-        self.assertEqual(screen,
+        screen = Screen(3, 7)
+        screen.rect(3, 2)
+        screen.rotate_column(1, 1)
+        screen.rotate_row(0, 4)
+        screen.rotate_column(1, 1)
+        self.assertEqual(screen.data,
                 [[False, True, False, False, True, False, True],
                  [True, False, True, False, False, False, False],
                  [False, True, False, False, False, False, False]])
@@ -76,8 +76,10 @@ class TestCountingLitPixels(unittest.TestCase):
     )
 
     def test_counts_lit_pixels(self):
-        for screen, expected in self.cases:
-            self.assertEqual(count_lit_pixels(screen), expected)
+        for data, expected in self.cases:
+            screen = Screen(3,2)
+            screen.data = data
+            self.assertEqual(screen.count_lit_pixels(), expected)
 
 
 if __name__ == '__main__':
